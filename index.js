1966f8768748f13a8e3b7b52bb09f36cdcb60fff
toxic-johann <353904974@qq.com>
2019-11-09 21:43:07 +0800

    feat: add one more selector

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const Benchmark = require('benchmark');
 const baseCategorySelector = ({ category }) => category;
 const contentModeSelector = ({ contentMode }) => contentMode;
 const categorySelector = createSelector(
+  (state) => state,
+  ({ category, contentMode }) => {
+    if (!contentMode) {
+      return category;
+    }
+    const { currentMode } = contentMode.map;
+    return currentMode ? contentMode[currentMode] : category;
+  }
+);
+const categorySelector2 = createSelector(
   baseCategorySelector,
   contentModeSelector,
   (category, contentMode) => {
@@ -43,14 +53,22 @@ function createState(size) {
         ? 'map'
         : faker.internet.email();
       state[key1][key2] = { currentMode: faker.address.streetAddress() };
+      if (key2 === 'map') {
+        state[key1][state[key1][key2].currentMode] = size;
+      }
     }
   }
   return state;
 }
 
+
 const state10 = createState(10);
 const state100 = createState(100);
 const state1000 = createState(1000);
+
+console.log(pureCategorySelector(state10), pureCategorySelector(state10) === categorySelector(state10))
+console.log(pureCategorySelector(state100), pureCategorySelector(state100) === categorySelector(state100))
+console.log(pureCategorySelector(state1000), pureCategorySelector(state1000) === categorySelector(state1000))
 
 const suite = new Benchmark.Suite();
 
@@ -69,6 +87,18 @@ suite
       }
     )
   })
+  .add('create selector  2       ', function() {
+    const categorySelector = createSelector(
+      (state) => state,
+      ({ category, contentMode }) => {
+        if (!contentMode) {
+          return category;
+        }
+        const { currentMode } = contentMode.map;
+        return currentMode ? contentMode[currentMode] : category;
+      }
+    );
+  })
   .add('create pure selector     ', function() {
     const pureCategorySelector = ({ category, contentMode }) => {
       if (!contentMode) {
@@ -81,17 +111,26 @@ suite
   .add('reslect             10*10', function() {
     categorySelector(state10);
   })
+  .add('reslect 2           10*10', function() {
+    categorySelector2(state10);
+  })
   .add('pure selector       10*10', function() {
     pureCategorySelector(state10);
   })
   .add('reslect           100*100', function() {
     categorySelector(state10);
   })
+  .add('reslect   2       100*100', function() {
+    categorySelector2(state10);
+  })
   .add('pure selector     100*100', function() {
     pureCategorySelector(state10);
   })
   .add('reslect         1000*1000', function() {
     categorySelector(state10);
+  })
+  .add('reslect    2    1000*1000', function() {
+    categorySelector2(state10);
   })
   .add('pure selector   1000*1000', function() {
     pureCategorySelector(state10);
